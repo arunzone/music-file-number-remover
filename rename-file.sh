9122b66caea4b7c577e45ea19c608c6691ac6e38
Arun <aparamasivam@aconex.com>
2017-12-29 11:29:28 +1100

    Support for m4a extension new pattern number followed bby space <12 abc> if condition removed by nehancing regex bubg fix: removes only numbers at the bbegining

--- rename-file.sh
+++ rename-file.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 echo "jousgua" | sed 's/\([oa]\)/X\1/g'
-regex_pattern="^.*\.(mp3|flac)$"
+regex_pattern="^.*\.(mp3|flac|m4a)$"
 find . -print0 | while read -d $'\0' file
 do
   if [[ "$file" =~ $regex_pattern ]]
   then
     file_name=$(echo "$file" | sed 's/^\.\///g')
-    file_name_no_num=$(echo "$file_name" | sed 's/^.*[ ]\-[ ]//g')
-    if [ "$file_name" == "$file_name_no_num" ]
-    then
-      file_name_no_num=$(echo "$file_name" | sed 's/^.*\.[ ]//g')
-    fi
-    echo "$file_name_no_num"
+    file_name_no_num=$(echo "$file_name" | sed 's/^[0-9]*\([ ]\)*\([ ]\-[ ]\)*\(\.[ ]\)*//')
+    echo "$file_name --> $file_name_no_num"
     mv "$file_name" "$file_name_no_num"
   fi
 done
